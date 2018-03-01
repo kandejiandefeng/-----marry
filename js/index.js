634dcb67f817ec026d294814b723b4bf62193fb8
kandejiandefeng <2653234756@qq.com>
2018-03-01 17:19:14 +0800

    -----marry

--- js/index.js
+++ js/index.js
@@ -1,38 +1,14 @@
-function main(){
-    oscroll();
-}
-main();
-function oscroll(){
-    var jj=document.getElementById('subnav');
-    window.onscroll=function () {
-        // var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
-        var subh =window.pageYOffset;
-        var hirght=120-subh;
-        if(subh>=120){
-            jj.style.top=0;
-        }else {
-            jj.style.top=hirght+'px';
-        }
-    };
-};
+$(function() {
+  
+    $('.j-tips').click(function(){
+        $('.j-tips').addClass('on').siblings().removeClass('on');
+    });
 
-// $(document).ready(function(){
-//     $('li#other').click(function(){
-//         $('div#other_sub').show();
-//     });
-//     // var oth=document.getElementById('other');
-//     // var oth_sub=document.getElementById('other_sub');
-//     // oth.click=function(){
-//     //     oth_sub.style.display = 'block';
-//     // }
-// });
-
-var myVideo=document.getElementById("video1"); 
-
-myVideo.onclick=function()
-{ 
-    if (myVideo.paused) 
-      myVideo.play(); 
-    else 
-      myVideo.pause(); 
-} 
+    var myVideo=document.getElementById("video1"); 
+        $('.show-per-v').click(function(){
+            if (myVideo.paused) 
+                  myVideo.play(); 
+                else 
+                  myVideo.pause(); 
+        });
+})
