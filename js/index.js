5d8026a89816bd5ddea908485c8383afdcc199a7
kandejiandefeng <2653234756@qq.com>
2018-03-02 00:14:24 +0800

    -----marry

--- js/index.js
+++ js/index.js
@@ -2,7 +2,15 @@
 $(function() {
   
     $('.j-tips').click(function(){
-        $('.j-tips').addClass('on').siblings().removeClass('on');
+        console.log(this);
+        $(this).addClass('on').siblings().removeClass('on');
+    });
+
+    $('.other').mouseenter(function(){
+        $(".o-direction").css('background-image','url(img/其他-2.png)')
+    });
+    $('.other').mouseleave(function(){
+        $(".o-direction").css('background-image','url(img/其他-1.png)')
     });
 
     var myVideo=document.getElementById("video1"); 
